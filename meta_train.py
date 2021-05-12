0db9eef93456c1450b1c524adc2edf45d7e268e1
Vikrant Yadav <v.yadav@elsevier.com>
2021-05-12 23:02:29 +0530

    Using config option

--- meta_train.py
+++ meta_train.py
@@ -32,14 +32,14 @@ def meta_train(config, model,df, device=torch.device("cpu")):
         train_encodings = tokenize(config, train_text)
         optimizer_ft=optim.SGD([
             {'params': model.bert.parameters()},
-            {'params': model.classifier.parameters(), 'lr': 1e-2}
-        ], lr=1e-7)
+            {'params': model.classifier.parameters(), 'lr': 2e-2}
+        ], lr=1e-5)
         training_steps = 20
         steps = 0
         model.train()
         data_loss = 0
         train_dataset = Dataset(train_encodings, task_data)
-        train_loader = DataLoader(train_dataset, batch_size=config["batchsize"], shuffle=config["shuffle"], \
+        train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=config["shuffle"], \
                                   num_workers=config["num_workers"], pin_memory=config["pin_memory"])
         for attention, input_id, token_id, label in tqdm(train_loader):
             if (torch.cuda.is_available()):
@@ -56,9 +56,9 @@ def meta_train(config, model,df, device=torch.device("cpu")):
             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             optimizer_ft.step()
             data_loss = data_loss + model_loss.item()
-            if (steps > 0 and steps % 4 == 0):
-                print(data_loss / 4)
-                writer.add_scalar(f'loss/task'+str(sample_task), data_loss/4, task_steps[sample_task])
+            if (steps > 0 and steps % 15 == 0):
+                print(data_loss / 15)
+                writer.add_scalar(f'loss/task'+str(sample_task), data_loss/15, task_steps[sample_task])
                 data_loss = 0
             steps = steps + 1
             task_steps[sample_task]=task_steps[sample_task]+1
