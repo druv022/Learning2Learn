d263f0ea8014b617e2994bcbea4704cbb0c5b12e
Vikrant Yadav <v.yadav@elsevier.com>
2021-06-14 20:25:58 +0200

    Update preprocess.py

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -2,6 +2,6 @@ from transformers import BertTokenizerFast
 
 def tokenize(config, texts):
     tokenizer = BertTokenizerFast.from_pretrained(config["modelname"])
-    encodings = tokenizer(texts, truncation=config["truncation"], padding=config["padding"],max_length=64)
+    encodings = tokenizer(texts, truncation=config["truncation"], padding=config["padding"],max_length=128)
     return encodings
 
