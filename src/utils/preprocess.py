3c15b8e41dc84462deb07b70ee055ebe9386169c
Vikrant Yadav <v.yadav@elsevier.com>
2021-06-20 22:51:01 +0200

    Update preprocess.py

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -10,17 +10,17 @@ class Tokenizer:
     def tokenize(self, texts, label_text=None):
         if label_text:
             encodings = self.tokenizer(
-                texts, label_text, truncation=self.config["truncation"], padding=self.config["padding"], max_length=128)
+                texts, label_text, truncation=self.config["truncation"], padding=self.config["padding"], max_length=256)
         else:
             encodings = self.tokenizer(
-                texts, truncation=self.config["truncation"], padding=self.config["padding"], max_length=128)
+                texts, truncation=self.config["truncation"], padding=self.config["padding"], max_length=256)
         return encodings
 
 
 def trim_text(text):
     text_arr = text.split(" ")
-    if(len(text_arr) > 110):
-        text_arr = text_arr[:110]
+    if(len(text_arr) > 230):
+        text_arr = text_arr[:230]
         st = ''
         for word in text_arr:
             st = st+word+' '
