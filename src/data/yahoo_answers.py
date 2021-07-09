214b6361169c96246f7f96e6c63978c9d781da73
Vikrant Yadav <v.yadav@elsevier.com>
2021-07-10 00:48:40 +0200

    Update for Yahoo

--- src/data/yahoo_answers.py
+++ src/data/yahoo_answers.py
@@ -74,7 +74,6 @@ class YahooAnswers14NLI(Dataset):
             self.sample_size = sample_size if len(self.dataset) > sample_size else len(self.dataset)
 
         self.num_labels = len(set(self.dataset['topic']))
-
         self.extended_labels = {i: config['prepend_topic'] + i.lower() if i.lower() not in config['yahoo_remapping'] else
                                 config['prepend_topic'] + config['yahoo_remapping'][i.lower()] for i in self.dataset.features['topic'].names}
 
